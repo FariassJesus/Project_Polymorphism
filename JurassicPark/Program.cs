6150fcb168e9bf5c25f8ac228e5e077b143bd838
Jesus <je-farias@hotmail.com>
2022-07-21 23:25:07 -0300

    Jurassic Park iniciado

---
+++ JurassicPark/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace JurassicPark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Bem vindo ao Jurassic Park");
+            WriteLine("Para Listar nossas atrações digite 1");
+            WriteLine("Para sair de nosso parque digite 2");
+
+            string opUser = ReadLine();
+
+            while (opUser == "1")
+            {
+                WriteLine("Escolha sua atração que deseja assistir");
+                WriteLine("Para dinos voadores digite 1");
+                WriteLine("Para dinos aquáticos digite 2");
+                WriteLine("Para dinos terrestres digite 3");
+
+                string opDino = ReadLine();
+
+                switch (opDino)
+                {
+                    case "1":
+                        
+                }
+            }
+        }
+    }
+}
